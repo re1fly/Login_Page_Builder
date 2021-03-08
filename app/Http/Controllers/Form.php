47d63bea819706f1d409f4d2f706eec67543a300
re1fly <reflyfa@gmail.com>
2021-03-08 08:37:36 +0800

    sending form

---
+++ app/Http/Controllers/Form.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Form extends Controller
+{
+    public function login(Request $request)
+    {
+        dd($request);
+    }
+}
